02da3685a70921b5d0a1f370d41d1c7eb200a82e
Kangsoo Lee <kangsoo.s.lee@gmail.com>
2017-07-10 13:16:29 +0900

    week2/chapter05

---
+++ week2/chapter05/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Kangsoo on 07/07/2017.
+//  Copyright © 2017 Dough. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
