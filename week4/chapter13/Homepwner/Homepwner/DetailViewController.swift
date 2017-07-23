fb7b0d375998ea171daf3f8bbd600aa1186c5ad8
Kangsoo Lee <kangsoo.s.lee@gmail.com>
2017-07-23 12:40:31 +0900

    Initialize chapter13

---
+++ week4/chapter13/Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,75 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Kangsoo on 22/07/2017.
+//  Copyright © 2017 Dough. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UITextFieldDelegate {
+	@IBOutlet var nameField: UITextField!
+	@IBOutlet var serialNumberField: UITextField!
+	@IBOutlet var valueField: UITextField!
+	@IBOutlet var dateLabel: UILabel!
+	
+	var item: Item! {
+		didSet {
+			guard item != nil else {
+				assertionFailure("The item is nil.")
+				return
+			}
+			navigationItem.title = item.name
+		}
+	}
+	
+	// MARK: 질문: Formatter는 상수로 쓰는게 좋을까? 아니면 함수로 만들어 쓰는게 좋을까?
+	let numberFormatter: NumberFormatter = {
+		let formatter = NumberFormatter()
+		formatter.numberStyle = .decimal
+		formatter.minimumFractionDigits = 2
+		formatter.maximumFractionDigits = 2
+		return formatter
+	}()
+	
+	let dateFormatter: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.dateStyle = .medium
+		formatter.timeStyle = .none
+		return formatter
+	}()
+	
+	@IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+		view.endEditing(true)
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		nameField.text = item.name
+		serialNumberField.text = item.serialNumber
+		valueField.text = numberFormatter.string(from: item.valueInDollars as NSNumber)
+		dateLabel.text = dateFormatter.string(from: item.dateCreated)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		view.endEditing(true)
+		
+		item.name = nameField.text ?? ""
+		item.serialNumber = serialNumberField.text
+		
+		guard let valueText = valueField.text,
+			let value = numberFormatter.number(from: valueText) else {
+				item.valueInDollars = 0
+				return
+		}
+		item.valueInDollars = value.intValue
+	}
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+}
---
+++ week4/chapter13/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Kangsoo on 16/07/2017.
+//  Copyright © 2017 Dough. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+	var itemStore : ItemStore! {
+		didSet {
+			guard itemStore != nil else {
+				assertionFailure("Item store is nil.")
+				return
+			}
+		}
+	}
+		
+    @IBAction func addNewItem(sender: AnyObject) {
+        let newItem = itemStore.createItem()
+        
+        guard let index = itemStore.allItems.index(of: newItem) else {
+			assertionFailure("An index of new item must not be nil.")
+			return
+		}
+        let indexPath = IndexPath(row: index, section: 0)
+    
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		
+		navigationItem.leftBarButtonItem = editButtonItem
+	}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+        makeCellHeightDynamic()
+    }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		tableView.reloadData()
+	}
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+			as? ItemCell ?? ItemCell(style: .default, reuseIdentifier: "ItemCell")
+			
+        cell.updateLabels()
+		
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+		cell.valueLabel.text = formatDollars(from: item.valueInDollars)
+		cell.setBackgroundColor(by: item.valueInDollars)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+		guard editingStyle == .delete else {
+			assertionFailure("Editing style is not 'delete'")
+			return
+		}
+		
+		let item = itemStore.allItems[indexPath.row]
+		
+		let title = "Delete \(item.name)?"
+		let message = "Are you sure you want to delete this item?"
+		let alertController = UIAlertController(title: title, message: message,
+		                                        preferredStyle: .actionSheet)
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+		alertController.addAction(cancelAction)
+		
+		let deleteAction = UIAlertAction(
+			title: "Delete",
+			style: .destructive,
+			handler: { (action) -> Void in
+			self.itemStore.removeItem(item: item)
+			self.tableView.deleteRows(at: [indexPath], with: .automatic)
+		})
+		alertController.addAction(deleteAction)
+		
+		present(alertController, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath,
+                            to destinationIndexPath: IndexPath) {
+        itemStore.moveItemAtIndex(fromIndex: sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		guard segue.identifier == "ShowItem" else {
+			print("The identifier of the segue object is not 'ShowItem'.")
+			return
+		}
+		
+		guard let row = tableView.indexPathForSelectedRow?.row else {
+			assertionFailure("An index path for the selected row is nil.")
+			return
+		}
+		if let detailViewController = segue.destination as? DetailViewController {
+			let item = itemStore.allItems[row]
+			detailViewController.item = item
+		}
+	}
+	
+	private func makeCellHeightDynamic() {
+		tableView.rowHeight = UITableViewAutomaticDimension
+		tableView.estimatedRowHeight = 65
+	}
+	
+	private func formatDollars(from value: Int) -> String {
+		let currencyFormatter = NumberFormatter()
+		currencyFormatter.numberStyle = .currency
+		currencyFormatter.locale = Locale(identifier: "en_US")
+		
+		guard let formattedValue = currencyFormatter.string(from: value as NSNumber) else {
+			assertionFailure("Failed to format currency")
+			return "N/A"
+		}
+		return formattedValue
+	}
+}
